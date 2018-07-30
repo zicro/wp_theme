8ef9f241b051fca582ef6e606da4269907b403ed
zicro <larhnimikit@gmail.com>
2018-07-30 14:56:13 +0100

    ..

--- index.php
+++ index.php
@@ -4,29 +4,64 @@ get_header();
 
 <div class="container">
 	<div class="row">
+	<?php
+		if (have_posts()) {
+			while (have_posts()) {
+				the_post();
+			
+	?>
 		<div class="col sm-6">
 			<div class="main-post">
-				<h3 class="post-title">this is our main Post title</h3>
-				<span class="post-author">Osman</span>
-				<span class="post-date">15/4/1254</span>
-				<span class="post-comments">20 cms</span>
-				<img class="img-responsive img-thumbnail" src="http://placehold.it/600x200/300" alt="" />
-				<p class="post-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h3 class="post-title">
+				<a href="<?php the_permalink() ?>"><?php the_title('')?></a>
+				</h3>
+				<span class="post-author"><?php the_author_posts_link() ?></span>
+				<span class="post-date"><?php the_time('F j, Y') ?></span><br>
+				<span class="post-comments"><?php comments_popup_link('0 Comments','1 comment','% comments') ?></span>
+				<?php the_post_thumbnail('',['class'=> 'img-responsive img-thumbnail']) ?>
+				<p class="post-content"><?php //the_content('{Read +++}') // used for show data (method 1)
+												the_excerpt(); ?></p>
 				<hr>
-			<p class="categories">
+			<p class="post-categories">
 				<i class="fas fa-tag"></i>
-				html, js, ps</p>
+				<?php the_category(', ') ?></p>
+			<p class="post-tags">
+				<i class="fas fa-tag"></i>
+				<?php 
+						if (has_tag()) {
+							the_tags();
+						}else{
+							echo "tags : none";
+						}
+
+				 ?></p>
 			</div>
 			
 		</div>
+		<?php
+			}
+		}
+		?>
+		<div class="clearfix"></div>
+		<div class="post-pagination">
+				<?php
+				// show previous and next page
+				if (get_previous_posts_link()) {
+					previous_posts_link('Prev ');
+				}else{
+					echo "<span class='prev-span'>Prev </span>";
+				}
+
+				if (get_next_posts_link()) {
+					next_posts_link(' Next');
+				}else{
+					echo "<span class='next-span'> Next</span>";
+				}
+				
+				?>
+		</div>
 	</div>
 </div>
-
 <?php
 get_footer();
 ?>
